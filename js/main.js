5d55ccd9400fa3a678a24189163d389e7e90bd3e
rubel hossain <rubel2585@gmail.com>
2016-12-05 00:50:34 +0600

    sufflejs with bootstrap added

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function($){
+
+  var shuffleme = (function($) {
+  'use strict';
+  var $grid = $('#grid'), //locate what we want to sort 
+      $filterOptions = $('.portfolio-sorting li'),  //locate the filter categories
+      $sizer = $grid.find('.shuffle_sizer'),    //sizer stores the size of the items
+};
+
+}(jQuery));
